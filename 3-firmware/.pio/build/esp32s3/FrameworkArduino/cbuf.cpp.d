88dda13c03b6022d850627c383bd8fb6cb1769ef
OLYPTEA <sachagibert4perso@gmail.com>
2026-05-21 17:56:41 +0200

    Correction bug + Nettoyage

--- 3-firmware/.pio/build/esp32s3/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32s3/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/minec/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/minec/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
